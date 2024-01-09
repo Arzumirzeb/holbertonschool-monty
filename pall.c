408cfe98f33fbd2a717c1aa8ce8fdc4ec837437f
arzumirzeb <arzumirzeb03@gmail.com>
2024-01-09 10:28:10 -0800

    Implementing the push and pall opcodes

---
+++ pall.c
@@ -0,0 +1,24 @@
+#include "monty.h"
+/**
+ * _pall - prints all the values on the stack, starting from the top
+ * @stack: pointer to the head of the stack
+ * @counter: line number
+ * Return: void
+ */
+void _pall(stack_t **stack, unsigned int counter)
+{
+	(void)counter;
+	int index = 0;
+	stack_t *temp;
+
+	temp = *stack;
+	while (temp)
+	{
+		if (temp != NULL)
+		{
+			printf("%d\n", temp->n);
+		}
+		index++;
+		temp = temp->next;
+	}
+}
---
+++ push.c
@@ -0,0 +1,39 @@
+#include "monty.h"
+
+/**
+ * _push - adds a new node at the beginning of a stack_t list.
+ * @opcode: opcode
+ * @stack: top of the stack
+ * @counter: line number
+ * Return: void
+ */
+void _push(char *opcode, stack_t **stack, unsigned int counter)
+{
+	stack_t *new;
+
+	new = malloc(sizeof(stack_t));
+
+	if (!new)
+	{
+		fprintf(stderr, "Error: malloc failed\n");
+		exit(EXIT_FAILURE);
+		return;
+	}
+	if (check_int(opcode) == 1 && opcode != NULL)
+	{
+		new->n = atoi(opcode);
+		new->next = *stack;
+		new->prev = NULL;
+	}
+	else
+	{
+		fprintf(stderr, "L%d usage: push integer\n", counter);
+		exit(EXIT_FAILURE);
+	}
+	if (*stack != NULL)
+	{
+		(*stack)->prev = new;
+	}
+	*stack = new;
+
+}
