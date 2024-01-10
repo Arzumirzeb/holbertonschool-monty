0ea33da19793ad32f4fc65ea2b0daa356e2c62be
Hesen58 <hesenisgenderov.hi.hi@gmail.com>
2024-01-10 12:46:02 -0800

    we'll see

--- execute.c
+++ execute.c
@@ -15,6 +15,7 @@ void exec_others(stack_t **stack, char *op, unsigned int line_number)
 								{"pop", pop},
 								{"swap", swap},
 								{"add", add},
+								{"nop", nop},
 								{NULL, NULL}
 	};
 
